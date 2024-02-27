7e643a593717185c882b8a7dac8e203be094a362
lucca vinicios <vini.lucca42@gmail.com>
2024-02-26 22:52:08 -0300

    feat: add health check use case

---
+++ src/domain/contracts/repositories/health_check_repository.py
@@ -0,0 +1,7 @@
+from abc import ABC, abstractmethod
+
+
+class IHealthCheckRepository(ABC):
+    @abstractmethod
+    async def is_available(self):
+        ...
---
+++ src/domain/use_cases/health_check.py
@@ -0,0 +1,19 @@
+import pydantic
+
+from dataclasses import dataclass
+from src.domain.contracts.repositories.health_check_repository import IHealthCheckRepository
+
+
+class OutputHealthCheckDTO(pydantic.BaseModel):
+    available: bool
+
+
+@dataclass
+class HealthCheckUseCase:
+    health_check_repository: IHealthCheckRepository
+
+    async def execute(self) -> OutputHealthCheckDTO:
+        service_is_available = await self.health_check_repository.is_available()
+        return OutputHealthCheckDTO(
+            available=service_is_available
+        )
